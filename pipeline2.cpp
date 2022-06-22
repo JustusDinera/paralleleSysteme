a1e0540e185946c3856d09d0d68fdf654e9f1125
Justus Dinera <frieden@freie-wahrheit.de>
2022-06-23 00:31:51 +0200

    pipeline2 did not work

--- pipeline2.cpp
+++ pipeline2.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
-#include <threads.h>
+#include <thread>
 #include <atomic>
 #include <math.h>
 #include <vector>
+#include <random>
 
 #define MAX_BUFFER 10
-
+#define ITERATION 10
+#define ARRAY_SIZE 1000
 
 class FIFO
 {
 private:
-    std::atomic<std::vector<float>> buffer;
+    float buffer[MAX_BUFFER];
+    uint counter = 0;
+    uint head = 0;
+    uint tail = 0;
 public:
     FIFO(/* args */);
     ~FIFO();
@@ -87,26 +92,53 @@ float FIFO::popItem()
 
 float calculation(float value)
 {
-    return (sin(value) * sin(value) * 100);
+    float retVal = 0;
+
+    for (int i = 0; i < ITERATION; i++)
+    {
+        retVal = sin(value) * 100;
+    }
+    
+    return retVal;
 }
 
-void stageFirst(FIFO & output, float value)
+// Thread functions
+void stageFirst(FIFO & output, float * value)
 {
+    auto startTime = std::chrono::steady_clock::now();
+    
     //cnd_wait(output.isFull,);
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        output.pushItem(calculation(*(value+i)));
+    }
 
-    output.pushItem(calculation(value));
+    printf("Thread 1 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
 }
 
 void stageMiddle(FIFO & input, FIFO & output)
 {
-    float retVal; 
+    float retVal;
+    auto startTime = std::chrono::steady_clock::now();
+    
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        retVal = calculation(input.popItem());
+        output.pushItem(retVal);
+    }
 
-    retVal = calculation(input.popItem());
-    output.pushItem(retVal);
+    printf("Thread 2 or 3 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
 }
-void stageLast()
+void stageLast(FIFO & input, float & output)
 {
+    auto startTime = std::chrono::steady_clock::now();
 
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        output = calculation(input.popItem());
+    }
+
+    printf("Thread 4 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
 }
 
 int main(int argc, char const *argv[])
@@ -115,6 +147,28 @@ int main(int argc, char const *argv[])
     FIFO fifo1;
     FIFO fifo2;
 
+    float startValues[ARRAY_SIZE];
+    float finalValues[ARRAY_SIZE];
+
+    // initial fill the start array 
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        startValues[i] = (float)rand();
+    }
+    
+    // Create threads
+    std::thread stateOne(stageFirst, fifo0, startValues);
+    std::thread stateTwo(stageMiddle, fifo0, fifo1);
+    std::thread stateThree(stageMiddle, fifo1, fifo2);
+    std::thread stateFour(stageFirst, fifo2, finalValues);
+
+    // Wait for all threads
+    stateOne.join();
+    stateTwo.join();
+    stateThree.join();
+    stateFour.join();
+
+    printf("All threads done!");
 
     return 0;
 }
