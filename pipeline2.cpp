500b8a6110ebfd7183138f7907ea2ab19c357fe1
Dinera <justus@darmsign.al>
2022-06-23 14:10:22 +0200

    pipeline2 done!

--- pipeline2.cpp
+++ pipeline2.cpp
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <fstream>
+#include <iomanip>
 #include <thread>
 #include <atomic>
 #include <math.h>
-#include <vector>
 #include <random>
 
 #define MAX_BUFFER 10
 #define ITERATION 10
 #define ARRAY_SIZE 1000
+#define MAX_DELAY 5
 
+/* class declaration */
 class FIFO
 {
 private:
     float buffer[MAX_BUFFER];
-    uint counter = 0;
-    uint head = 0;
-    uint tail = 0;
+    std::atomic<uint> counter;
+    uint head;
+    uint tail;
 public:
-    FIFO(/* args */);
+    FIFO();
     ~FIFO();
     bool isFull();
     bool isEmpty();
@@ -25,8 +28,11 @@ public:
     float popItem();
 };
 
-FIFO::FIFO(/* args */)
+FIFO::FIFO()
 {
+    counter = 0;
+    head = 0;
+    tail = 0;
 }
 
 FIFO::~FIFO()
@@ -61,15 +67,16 @@ void FIFO::pushItem(float item)
 {
     if (!isFull())
     {
-        counter++;
-        
         buffer[head] = item;
 
         head++;
+        // handle an overflow
         if (head == MAX_BUFFER)
         {
             head = 0;
         }
+
+        counter++;
     }
 }
 
@@ -77,20 +84,28 @@ float FIFO::popItem()
 {
     float retVal;
     
-    counter--;
     retVal = buffer[tail];
 
-
     tail++;
+    //handle an overflow
     if (tail == MAX_BUFFER)
     {
         tail = 0;
     }
     
+    counter--;
     return retVal;    
 }
 
-float calculation(float value)
+/* Variable declaration */
+FIFO fifo0;
+FIFO fifo1;
+FIFO fifo2;
+float startValues[ARRAY_SIZE];
+float finalValues[ARRAY_SIZE];
+
+/* function declaration */
+float calculation1(float value)
 {
     float retVal = 0;
 
@@ -102,71 +117,162 @@ float calculation(float value)
     return retVal;
 }
 
+float calculation2(float value)
+{
+    float retVal = 0;
+
+    for (int i = 0; i < ITERATION; i++)
+    {
+        retVal = sin(value) * cos(value) * 100;
+    }
+    
+    return retVal;
+}
+
+float calculation3(float value)
+{
+    float retVal = 0;
+
+    for (int i = 0; i < ITERATION; i++)
+    {
+        retVal = cos(value) * 100;
+    }
+    
+    return retVal;
+}
+
 // Thread functions
-void stageFirst(FIFO & output, float * value)
+void stageFirst()
 {
     auto startTime = std::chrono::steady_clock::now();
     
-    //cnd_wait(output.isFull,);
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
-        output.pushItem(calculation(*(value+i)));
+        // wait in case fo a full output buffer
+        while (fifo0.isFull());        
+        // push value to the output buffer
+        fifo0.pushItem(calculation1(startValues[i]));
+
+        if (i < ARRAY_SIZE -1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand()%MAX_DELAY));
+        }
+        
     }
 
-    printf("Thread 1 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
+    printf("Thread 1 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - startTime).count());
 }
 
-void stageMiddle(FIFO & input, FIFO & output)
+void stageMiddle1()
 {
     float retVal;
     auto startTime = std::chrono::steady_clock::now();
     
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
-        retVal = calculation(input.popItem());
-        output.pushItem(retVal);
+        // wait in case of empty input buffer
+        while (fifo0.isEmpty());
+        // pop out the current value
+        retVal = calculation2(fifo0.popItem());
+
+        // wait in case fo a full output buffer
+        while (fifo1.isFull());
+        // push value to the output buffer
+        fifo1.pushItem(retVal);
+
+        if (i < ARRAY_SIZE -1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand()%MAX_DELAY));
+        }
     }
 
-    printf("Thread 2 or 3 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
+    printf("Thread 2 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - startTime).count());
 }
-void stageLast(FIFO & input, float & output)
+
+void stageMiddle2()
+{
+    float retVal;
+    auto startTime = std::chrono::steady_clock::now();
+    
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        // wait in case of empty input buffer
+        while (fifo1.isEmpty());
+        retVal = calculation3(fifo1.popItem());
+
+        // wait in case fo a full output buffer
+        while (fifo2.isFull());
+        // push value to the output buffer
+        fifo2.pushItem(retVal);
+        
+        if (i < ARRAY_SIZE -1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand()%MAX_DELAY));
+        }
+    }
+
+    printf("Thread 3 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - startTime).count());
+}
+
+void stageLast()
 {
     auto startTime = std::chrono::steady_clock::now();
 
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
-        output = calculation(input.popItem());
+        // wait in case of empty buffer
+        while (fifo2.isEmpty());
+        // write final values in the output array
+        finalValues[i] = calculation1(fifo2.popItem());
+
+        // delay the thread to provocate runtime issues
+        if (i < ARRAY_SIZE -1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(rand()%MAX_DELAY));
+        }
     }
 
-    printf("Thread 4 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(startTime - std::chrono::steady_clock::now()).count());
+    printf("Thread 4 is done with a duration of %ius!\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - startTime).count());
 }
 
 int main(int argc, char const *argv[])
 {
-    FIFO fifo0;
-    FIFO fifo1;
-    FIFO fifo2;
+    std::ofstream outputFile;
+    outputFile.open("Arrays.txt");
 
-    float startValues[ARRAY_SIZE];
-    float finalValues[ARRAY_SIZE];
+    outputFile << "--- Input Array ---" << std::endl;
 
-    // initial fill the start array 
+    // initial fill the start array and print the array to file
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
         startValues[i] = (float)rand();
+
+        outputFile << startValues[i] << '\t';
+        if ((i%5 == 0)&&(i != 0))
+            outputFile << std::endl;        
     }
     
+
+
     // Create threads
-    std::thread stateOne(stageFirst, fifo0, startValues);
-    std::thread stateTwo(stageMiddle, fifo0, fifo1);
-    std::thread stateThree(stageMiddle, fifo1, fifo2);
-    std::thread stateFour(stageFirst, fifo2, finalValues);
+    std::thread stateOne(stageFirst);
+    std::thread stateTwo(stageMiddle1);
+    std::thread stateThree(stageMiddle2);
+    std::thread stateFour(stageLast);
 
     // Wait for all threads
     stateOne.join();
     stateTwo.join();
     stateThree.join();
     stateFour.join();
+
+    outputFile << "\n--- Output Array ---" << std::endl;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        outputFile << std::setprecision(5) << finalValues[i] << '\t';
+        if ((i%5 == 0)&&(i != 0))
+            outputFile << std::endl;        
+    }
 
     printf("All threads done!");
 
